cff35d58bc20187145f11fd11f186abe64f95f17
MOSKALCHUK <moskalchuk@MacBook-Air-MOSKALCHUK.local>
2022-08-06 00:21:38 +0300

    Add first routing

--- src/main.js
+++ src/main.js
@@ -4,14 +4,25 @@ import { createApp } from 'vue';
 import {createRouter, createWebHistory} from 'vue-router';
 
 import App from './App.vue';
+// Import components, which have to be used in routing
+import UsersList from './components/users/UsersList';
+import TeamsList from './components/teams/TeamsList';
 
 // Create router
-
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    // Первый параметр - ссылка [https://our-domain.com/teams]
+    // Второй параметр - компонент, который мы импортировали
+    // Если мы импортируем их в роуты, то в App.vue нам не нужно их определять
+    routes: [
+        {path: '/teams', component: TeamsList},
+        {path: '/users', component: UsersList}
+    ]
 });
 
 const app = createApp(App);
+
+// Функция use для того чтобы использовать роуты
+app.use(router);
 
 app.mount('#app');
