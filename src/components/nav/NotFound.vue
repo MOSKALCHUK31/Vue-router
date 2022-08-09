e476a89fcdc5acfd3afe07ac380335d5c3ef99f9
MOSKALCHUK <moskalchuk@MacBook-Air-MOSKALCHUK.local>
2022-08-10 00:32:57 +0300

    Redirecting & Catch all invalid routes

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>The path isn't supported. Maybe you want to be redirected to <router-link to="/teams">Teams</router-link> component?</h2>
+</template>
--- src/main.js
+++ src/main.js
@@ -6,13 +6,24 @@ import App from './App.vue';
 import UsersList from './components/users/UsersList';
 import TeamsList from './components/teams/TeamsList';
 import TeamMembers from './components/teams/TeamMembers';
+import NotFound from './components/nav/NotFound';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/teams', component: TeamsList},
+        // Редирект. Если / , то идет в компонент /teams
+        // Меняется ЮРЛ
+        // {path: '/', redirect: '/teams'},
+        // Используем альтернативный юрл как / для того чтобы установить по дефолту 
+        // /teams
+        {path: '/teams', component: TeamsList, alias: '/'},
         {path: '/users', component: UsersList},
-        {path: '/teams/:teamId', component: TeamMembers, props: true}
+        {path: '/teams/:teamId', component: TeamMembers, props: true},
+        // Редиректим на компонент, если ЮРЛ инвалдиный (такого нет)
+        // {path: '/:notFound(.*)', redirect: '/teams'}
+        //
+        {path: '/:notFound(.*)', component: NotFound}
+        
     ],
     linkActiveClass: 'active'
 });
