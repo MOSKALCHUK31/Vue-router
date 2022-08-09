70be8e24f67c91e7973b01b204f807e32fcc0108
MOSKALCHUK <moskalchuk@MacBook-Air-MOSKALCHUK.local>
2022-08-09 22:32:18 +0300

    Passing data with route params

--- src/main.js
+++ src/main.js
@@ -5,16 +5,15 @@ import {createRouter, createWebHistory} from 'vue-router';
 import App from './App.vue';
 import UsersList from './components/users/UsersList';
 import TeamsList from './components/teams/TeamsList';
+import TeamMembers from './components/teams/TeamMembers';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/teams', component: TeamsList},
-        {path: '/users', component: UsersList}
+        {path: '/users', component: UsersList},
+        {path: '/teams/:teamId', component: TeamMembers}
     ],
-    // По дефолту (можно не писать)
-    // linkActiveClass: 'router-link-active'
-    // Переопределение активного класа
     linkActiveClass: 'active'
 });
 
